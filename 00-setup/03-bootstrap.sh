7c6d0c59a58959a4a36b064a8f73e3e97bca4078
Pierre Constantineau <jpconstantineau@gmail.com>
2022-03-03 22:05:29 -0600

    updates

---
+++ 00-setup/03-bootstrap.sh
@@ -0,0 +1,7 @@
+export GITHUB_TOKEN=<your-github-token>
+
+flux bootstrap github \
+  --owner=jpconstantineau \
+  --repository=hybrid-lab-setup \
+  --path=clusters/oci \
+  --personal
--- setup-ansible/install-wsl.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install -y ansible sshpass
-
-ansible --version
-
-git config --global user.name "Pierre Constantineau"
-git config --global user.email "jpconstantineau@gmail.com"
