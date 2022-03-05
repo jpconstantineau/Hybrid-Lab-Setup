4a78c885231b42e02d06d140e385c3672780319e
Pierre Constantineau <jpconstantineau@gmail.com>
2022-03-04 23:12:34 -0600

    k3sup

--- 00-setup/01-oci3-k3s.sh
+++ 00-setup/01-oci3-k3s.sh
@@ -6,9 +6,9 @@ set -e
 
 export K8SVERSION=v1.23.3+k3s1
 
-export NODE_1=10.0.0.235
-export NODE_2=10.0.0.180
-export NODE_3=10.0.0.223
+export NODE_1=172.16.0.1
+export NODE_2=172.16.0.2
+export NODE_3=172.16.0.3
 export USER=ubuntu
 
 
